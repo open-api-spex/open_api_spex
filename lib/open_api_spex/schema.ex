e99b5c74ca825237b893f881dafc0d8c865ae39f
Michael Buhot <m.buhot@gmail.com>
2018-06-28 21:32:15 +1000

    WIP - cast polymorphic schemas using discriminator and allOf

--- lib/open_api_spex/schema.ex
+++ lib/open_api_spex/schema.ex
@@ -136,7 +136,10 @@ defmodule OpenApiSpex.Schema do
   @spec cast(Schema.t | Reference.t, any, %{String.t => Schema.t | Reference.t}) :: {:ok, any} | {:error, String.t}
   def cast(schema = %Schema{"x-struct": mod}, value, schemas) when not is_nil(mod) do
     with {:ok, data} <- cast(%{schema | "x-struct": nil}, value, schemas) do
-      {:ok, struct(mod, data)}
+      case data do
+        %{__struct__: _} -> {:ok, data}
+        _ -> {:ok, struct(mod, data)}
+      end
     end
   end
   def cast(%Schema{type: :boolean}, value, _schemas) when is_boolean(value), do: {:ok, value}
@@ -185,9 +188,29 @@ defmodule OpenApiSpex.Schema do
   def cast(%Schema{type: :array}, value, _schemas) when not is_list(value) do
     {:error, "Invalid array: #{inspect(value)}"}
   end
+  def cast(schema = %Schema{type: :object, discriminator: discriminator = %{}}, value = %{}, schemas) do
+    with {:ok, value} <- tag(schema, value),
+         {:ok, derived_schema} <- Discriminator.resolve(discriminator, value, schemas),
+         {:ok, result} <- cast(derived_schema, value, schemas) do
+      {:ok, untag(schema, result)}
+    else
+      {:error, :already_tagged} -> {:ok, %{}}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+  def cast(%Schema{type: :object, allOf: [x | rest]}, value = %{}, schemas) do
+    with {:ok, cast_x} <- cast(x, value, schemas),
+         {:ok, cast_rest} <- cast(rest, value, schemas) do
+      Map.merge(cast_x, cast_rest)
+    end
+  end
+  def cast(schema = %Schema{type: :object, allOf: []}, value = %{}, schemas) do
+    cast(%{schema | allOf: nil}, value, schemas)
+  end
   def cast(schema = %Schema{type: :object}, value, schemas) when is_map(value) do
     schema = %{schema | properties: schema.properties || %{}}
-    with {:ok, props} <- cast_properties(schema, Enum.to_list(value), schemas) do
+    regular_properties = Enum.filter(value, fn {k, _v} -> is_binary(k) end)
+    with {:ok, props} <- cast_properties(schema, regular_properties, schemas) do
       {:ok, Map.new(props)}
     end
   end
@@ -208,6 +231,29 @@ defmodule OpenApiSpex.Schema do
     with {:ok, new_value} <- cast(schema, value, schemas),
          {:ok, cast_tail} <- cast_properties(object_schema, rest, schemas) do
       {:ok, [{name, new_value} | cast_tail]}
+    end
+  end
+
+  # Record that a schema has already been used for casting/validation on a value
+  @spec tag(Schema.t, map) :: {:ok, map} | {:error, :already_tagged}
+  defp tag(schema, value) do
+    value = Map.put_new(value, :__open_api_spex_tags, [])
+    tags = value.__open_api_spex_tags
+    if schema.title in tags do
+      {:error, :already_tagged}
+    else
+      {:ok, %{value | tags: [schema.title | tags]}}
+    end
+  end
+
+  # Remove previously recorded tag
+  @spec untag(Schema.t, map) :: map
+  defp untag(schema, value) do
+    tags = value.__open_api_spex_tags -- [schema.title]
+    if Enum.empty?(tags) do
+      Map.delete(value, :__open_api_spex_tags)
+    else
+      Map.put(value, :__open_api_spex_tags, tags)
     end
   end
 
