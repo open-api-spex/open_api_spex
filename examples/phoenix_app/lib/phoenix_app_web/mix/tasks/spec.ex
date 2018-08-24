4cb18bdbc87684c120725fc8a2603ea7e7991329
Pierre Fenoll <pierrefenoll@gmail.com>
2018-08-24 17:08:48 +0200

    add mix task

---
+++ examples/phoenix_app/lib/phoenix_app_web/mix/tasks/spec.ex
@@ -0,0 +1,7 @@
+defmodule Mix.Tasks.PhoenixAppWeb.OpenApiSpec do
+  def run([]) do
+    PhoenixAppWeb.ApiSpec.spec()
+    |> Poison.encode!(pretty: true, strict_keys: true)
+    |> (&File.write!("openapi3_v1.0.yml", &1)).()
+  end
+end
