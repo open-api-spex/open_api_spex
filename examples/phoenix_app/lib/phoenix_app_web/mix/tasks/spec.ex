2007b2d3fff5c38e90eb16aea6072e7ce77ce41f
Pierre Fenoll <pierrefenoll@gmail.com>
2018-08-27 10:26:13 +0200

    woops, we're writtign json not yaml

--- examples/phoenix_app/lib/phoenix_app_web/mix/tasks/spec.ex
+++ examples/phoenix_app/lib/phoenix_app_web/mix/tasks/spec.ex
@@ -2,6 +2,6 @@ defmodule Mix.Tasks.PhoenixAppWeb.OpenApiSpec do
   def run([]) do
     PhoenixAppWeb.ApiSpec.spec()
     |> Poison.encode!(pretty: true, strict_keys: true)
-    |> (&File.write!("openapi3_v1.0.yml", &1)).()
+    |> (&File.write!("openapi3_v1.0.json", &1)).()
   end
 end
