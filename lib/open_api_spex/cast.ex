e3189c80a0b7268fcab9804e47e18c832cfab42e
Moxley Stratton <moxley.stratton@gmail.com>
2018-11-17 15:13:42 -0800

    Cast.cast with primitives, array

---
+++ lib/open_api_spex/cast.ex
@@ -0,0 +1,42 @@
+defmodule OpenApiSpex.Cast do
+  alias OpenApiSpex.{CastPrimitive, Error}
+  @primitives [:boolean, :integer, :number, :string]
+
+  def cast(value, schema, schemas \\ nil)
+
+  def cast(value, nil, _schemas),
+    do: {:ok, value}
+
+  def cast(value, %{type: :array} = schema, schemas) do
+    cast_array(value, schema, schemas)
+  end
+
+  def cast(value, %{type: type} = schema, _schemas) when type in @primitives do
+    CastPrimitive.cast(value, schema)
+  end
+
+  ## Private functions
+
+  defp cast_array(_value, _schema, _schemas, index \\ 0)
+
+  defp cast_array([], _schema, _schemas, _index) do
+    {:ok, []}
+  end
+
+  defp cast_array([item | rest], schema, schemas, index) do
+    with {:ok, cast_item} <- cast_array_item(item, schema.items, schemas, index),
+         {:ok, cast_rest} <- cast_array(rest, schema, schemas, index + 1) do
+      {:ok, [cast_item | cast_rest]}
+    end
+  end
+
+  defp cast_array(value, _schema, _schemas, _index) do
+    {:error, Error.new(:invalid_type, :array, value)}
+  end
+
+  defp cast_array_item(item, items_schema, schemas, index) do
+    with {:error, error} <- cast(item, items_schema, schemas) do
+      {:error, %{error | path: [index | error.path]}}
+    end
+  end
+end
---
+++ test/cast_test.exs
@@ -0,0 +1,54 @@
+defmodule OpenApiSpec.CastTest do
+  use ExUnit.Case
+  import OpenApiSpex.Cast
+  alias OpenApiSpex.{Error, Schema}
+
+  describe "cast/3" do
+    # Note: full tests for primitives are covered in CastPrimitiveTest
+    test "primitives" do
+      tests = [
+        {:string, "1", :ok},
+        {:string, "", :ok},
+        {:string, true, :invalid},
+        {:string, nil, :invalid},
+        {:integer, 1, :ok},
+        {:integer, "1", :ok},
+        {:integer, %{}, :invalid},
+        {:integer, nil, :invalid}
+      ]
+
+      for {type, input, expected} <- tests do
+        case expected do
+          :ok -> assert {:ok, _} = cast(input, %Schema{type: type})
+          :invalid -> assert {:error, _} = cast(input, %Schema{type: type})
+        end
+      end
+    end
+
+    test "array" do
+      schema = %Schema{type: :array}
+      assert cast([], schema) == {:ok, []}
+      assert cast([1, 2, 3], schema) == {:ok, [1, 2, 3]}
+      assert cast(["1", "2", "3"], schema) == {:ok, ["1", "2", "3"]}
+
+      assert {:error, error} = cast(%{}, schema)
+      assert %Error{} = error
+      assert error.reason == :invalid_type
+      assert error.value == %{}
+    end
+
+    test "array with items schema" do
+      items_schema = %Schema{type: :integer}
+      schema = %Schema{type: :array, items: items_schema}
+      assert cast([], schema) == {:ok, []}
+      assert cast([1, 2, 3], schema) == {:ok, [1, 2, 3]}
+      assert cast(["1", "2", "3"], schema) == {:ok, [1, 2, 3]}
+
+      assert {:error, error} = cast([1, "two"], schema)
+      assert %Error{} = error
+      assert error.reason == :invalid_type
+      assert error.value == "two"
+      assert error.path == [1]
+    end
+  end
+end
