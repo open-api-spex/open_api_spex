4912b5d8f46551143642d1bf15a58327564327c3
Moxley Stratton <moxley.stratton@gmail.com>
2018-11-17 17:14:02 -0800

    Break out CastArray module

--- lib/open_api_spex/cast.ex
+++ lib/open_api_spex/cast.ex
@@ -1,46 +1,18 @@
 defmodule OpenApiSpex.Cast do
-  alias OpenApiSpex.{CastObject, CastPrimitive, Error}
+  alias OpenApiSpex.{CastArray, CastObject, CastPrimitive}
   @primitives [:boolean, :integer, :number, :string]
 
   def cast(value, schema, schemas \\ nil)
 
-  def cast(value, nil, _schemas),
-    do: {:ok, value}
+  def cast(value, nil, _schemas), do: {:ok, value}
 
   def cast(value, %{type: type} = schema, _schemas) when type in @primitives do
     CastPrimitive.cast(value, schema)
   end
 
   def cast(value, %{type: :array} = schema, schemas),
-    do: cast_array(value, schema, schemas)
+    do: CastArray.cast(value, schema, schemas)
 
   def cast(value, %{type: :object} = schema, schemas),
     do: CastObject.cast(value, schema, schemas)
-
-  ## Private functions
-
-  ## Array
-
-  defp cast_array(_value, _schema, _schemas, index \\ 0)
-
-  defp cast_array([], _schema, _schemas, _index) do
-    {:ok, []}
-  end
-
-  defp cast_array([item | rest], schema, schemas, index) do
-    with {:ok, cast_item} <- cast_array_item(item, schema.items, schemas, index),
-         {:ok, cast_rest} <- cast_array(rest, schema, schemas, index + 1) do
-      {:ok, [cast_item | cast_rest]}
-    end
-  end
-
-  defp cast_array(value, _schema, _schemas, _index) do
-    {:error, Error.new(:invalid_type, :array, value)}
-  end
-
-  defp cast_array_item(item, items_schema, schemas, index) do
-    with {:error, error} <- cast(item, items_schema, schemas) do
-      {:error, %{error | path: [index | error.path]}}
-    end
-  end
 end
---
+++ lib/open_api_spex/cast_array.ex
@@ -0,0 +1,27 @@
+defmodule OpenApiSpex.CastArray do
+  @moduledoc false
+  alias OpenApiSpex.{Cast, Error}
+
+  def cast(_value, _schema, _schemas, index \\ 0)
+
+  def cast([], _schema, _schemas, _index) do
+    {:ok, []}
+  end
+
+  def cast([item | rest], schema, schemas, index) do
+    with {:ok, cast_item} <- cast_item(item, schema.items, schemas, index),
+         {:ok, cast_rest} <- cast(rest, schema, schemas, index + 1) do
+      {:ok, [cast_item | cast_rest]}
+    end
+  end
+
+  def cast(value, _schema, _schemas, _index) do
+    {:error, Error.new(:invalid_type, :array, value)}
+  end
+
+  defp cast_item(item, items_schema, schemas, index) do
+    with {:error, error} <- Cast.cast(item, items_schema, schemas) do
+      {:error, %{error | path: [index | error.path]}}
+    end
+  end
+end
