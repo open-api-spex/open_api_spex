95165160bec8ce83e9b2957a974486474b5299c5
Moxley Stratton <moxley.stratton@gmail.com>
2018-11-18 18:55:27 -0800

    Small refactor in CastString

--- lib/open_api_spex/cast_string.ex
+++ lib/open_api_spex/cast_string.ex
@@ -2,15 +2,6 @@ defmodule OpenApiSpex.CastString do
   @moduledoc false
   alias OpenApiSpex.CastContext
 
-  def cast(%{value: value, schema: %{pattern: pattern}} = ctx)
-      when not is_nil(pattern) and is_binary(value) do
-    if Regex.match?(pattern, value) do
-      {:ok, value}
-    else
-      CastContext.error(ctx, {:invalid_format, pattern})
-    end
-  end
-
   def cast(%{value: value} = ctx) when is_binary(value) do
     cast_binary(ctx)
   end
@@ -20,6 +11,14 @@ defmodule OpenApiSpex.CastString do
   end
 
   ## Private functions
+
+  defp cast_binary(%{value: value, schema: %{pattern: pattern}} = ctx) when not is_nil(pattern) do
+    if Regex.match?(pattern, value) do
+      {:ok, value}
+    else
+      CastContext.error(ctx, {:invalid_format, pattern})
+    end
+  end
 
   defp cast_binary(%{value: value, schema: %{minLength: min_length}} = ctx)
        when is_integer(min_length) do
