4736082b29d37740d5e1fef200570a7125ec48f8
Moxley Stratton <moxley.stratton@gmail.com>
2018-12-07 11:58:57 -0800

    [SRVC-4463] Set up test for Operation2.cast/4
    This leads up to testing and fixing the query parameters cast/validation

---
+++ test/operation2_test.exs
@@ -0,0 +1,90 @@
+defmodule OpenApiSpex.Operation2Test do
+  use ExUnit.Case
+  alias OpenApiSpex.{Operation, Operation2, Schema}
+
+  defmodule SchemaFixtures do
+    @user %Schema{
+      type: :object,
+      properties: %{
+        user: %Schema{
+          type: :object,
+          properties: %{
+            email: %Schema{type: :string}
+          }
+        }
+      }
+    }
+    @schemas %{"User" => @user}
+
+    def user, do: @user
+    def schemas, do: @schemas
+  end
+
+  defmodule OperationFixtures do
+    @create_user %Operation{
+      operationId: "UserController.create",
+      requestBody:
+        Operation.request_body("request body", "application/json", SchemaFixtures.user(),
+          required: true
+        ),
+      responses: %{
+        200 => Operation.response("User", "application/json", SchemaFixtures.user())
+      }
+    }
+
+    def create_user, do: @create_user
+  end
+
+  defmodule SpecModule do
+    def spec do
+      paths = %{
+        "/users" => %{
+          "post" => OperationFixtures.create_user()
+        }
+      }
+
+      %OpenApiSpex.OpenApi{
+        info: nil,
+        paths: paths,
+        components: %{
+          schemas: SchemaFixtures.schemas()
+        }
+      }
+    end
+  end
+
+  defmodule RenderError do
+    def init(_) do
+      nil
+    end
+
+    def call(_conn, _errors) do
+      raise "should not have errors"
+    end
+  end
+
+  describe "cast/4" do
+    test "cast request body" do
+      conn = create_conn(%{"user" => %{"email" => "foo@bar.com"}})
+
+      assert {:ok, value} =
+               Operation2.cast(
+                 OperationFixtures.create_user(),
+                 conn,
+                 "application/json",
+                 SchemaFixtures.schemas()
+               )
+
+      assert %Plug.Conn{} = value
+    end
+
+    # TODO Verify query params are casted
+
+    defp create_conn(body_params) do
+      :post
+      |> Plug.Test.conn("/api/users")
+      |> Plug.Conn.put_req_header("content-type", "application/json")
+      |> Map.put(:body_params, body_params)
+    end
+  end
+end
