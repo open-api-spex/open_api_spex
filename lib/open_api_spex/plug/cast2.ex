1f5160d92309537bda895c315e0968304d0334da
Moxley Stratton <moxley.stratton@gmail.com>
2019-02-22 09:28:49 -0800

    Use @deprecated instead

--- lib/open_api_spex/plug/cast2.ex
+++ lib/open_api_spex/plug/cast2.ex
@@ -2,14 +2,10 @@ defmodule OpenApiSpex.Plug.Cast2 do
   @behaviour Plug
 
   @impl Plug
-  def init(opts) do
-    IO.warn("OpenApiSpex.Plug.Cast2 is deprecated. Please use OpenApiSpex.Plug.CastAndValidate.")
-    OpenApiSpex.Plug.CastAndValidate.init(opts)
-  end
+  @deprecated "Use OpenApiSpex.Plug.CastAndValidate.init/1 instead"
+  defdelegate init(opts), to: OpenApiSpex.Plug.CastAndValidate
 
   @impl Plug
-  def call(conn, opts) do
-    IO.warn("OpenApiSpex.Plug.Cast2 is deprecated. Please use OpenApiSpex.Plug.CastAndValidate.")
-    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
-  end
+  @deprecated "Use OpenApiSpex.Plug.CastAndValidate.call/2 instead"
+  defdelegate call(conn, opts), to: OpenApiSpex.Plug.CastAndValidate
 end
