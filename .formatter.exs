4e8c7d699315a6766af2ff703474f0480ad9423a
Moxley Stratton <moxley.stratton@gmail.com>
2019-03-13 07:53:49 -0700

    Adjustments to .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,19 +1,10 @@
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,rel,test}/**/*.{ex,exs}"],
-  import_deps: [:plug],
+  inputs: ["{mix,.formatter}.exs", "{config,lib,rel,test,examples}/**/*.{ex,exs}"],
+  import_deps: [:plug, :phoenix],
   locals_without_parens: [
     transport: 2,
     action_fallback: 1,
     socket: 2,
-    render: 2,
-
-    # Distillery
-    set: 1,
-
-    # Phoenix.
-    pipe_through: 1,
-    head: 3,
-    get: 3,
-    post: 3
+    render: 2
   ]
 ]
