7234ae1ef18c5e8f9f3c4ee288fed108005d3edf
Moxley Stratton <moxley.stratton@gmail.com>
2019-03-14 08:18:56 -0700

    Move tests to own module

---
+++ test/plug/cast_and_validate/custom_error_user_controller_test.exs
@@ -0,0 +1,39 @@
+defmodule OpenApiSpex.Plug.CastAndValidate.CustomErrorUserControllerTest do
+  use ExUnit.Case, async: true
+
+  describe "query params - param with custom error handling" do
+    test "Valid Param" do
+      conn =
+        :get
+        |> Plug.Test.conn("/api/cast_and_validate_test/custom_error_users?validParam=true")
+        |> OpenApiSpexTest.Router.call([])
+
+      assert conn.status == 200
+    end
+
+    @tag :capture_log
+    test "Invalid value" do
+      conn =
+        :get
+        |> Plug.Test.conn("/api/cast_and_validate_test/custom_error_users?validParam=123")
+        |> OpenApiSpexTest.Router.call([])
+
+      assert conn.status == 400
+
+      assert conn.resp_body == "Invalid boolean. Got: string"
+    end
+
+    @tag :capture_log
+    test "Invalid Param" do
+      conn =
+        :get
+        |> Plug.Test.conn(
+          "/api/cast_and_validate_test/custom_error_users?validParam=123&inValidParam=123&inValid2=hi"
+        )
+        |> OpenApiSpexTest.Router.call([])
+
+      assert conn.status == 400
+      assert conn.resp_body == "Unexpected field: inValid2"
+    end
+  end
+end
--- test/plug/cast_and_validate_test.exs
+++ test/plug/cast_and_validate_test.exs
@@ -1,5 +1,5 @@
 defmodule OpenApiSpex.Plug.CastAndValidateTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   describe "query params - basics" do
     test "Valid Param" do
@@ -51,43 +51,6 @@ defmodule OpenApiSpex.Plug.CastAndValidateTest do
         |> OpenApiSpexTest.Router.call([])
 
       assert conn.status == 200
-    end
-  end
-
-  # TODO Move to new file: cast_and_validate/custom_error_user_controller_test.exs
-  describe "query params - param with custom error handling" do
-    test "Valid Param" do
-      conn =
-        :get
-        |> Plug.Test.conn("/api/cast_and_validate_test/custom_error_users?validParam=true")
-        |> OpenApiSpexTest.Router.call([])
-
-      assert conn.status == 200
-    end
-
-    @tag :capture_log
-    test "Invalid value" do
-      conn =
-        :get
-        |> Plug.Test.conn("/api/cast_and_validate_test/custom_error_users?validParam=123")
-        |> OpenApiSpexTest.Router.call([])
-
-      assert conn.status == 400
-
-      assert conn.resp_body == "Invalid boolean. Got: string"
-    end
-
-    @tag :capture_log
-    test "Invalid Param" do
-      conn =
-        :get
-        |> Plug.Test.conn(
-          "/api/cast_and_validate_test/custom_error_users?validParam=123&inValidParam=123&inValid2=hi"
-        )
-        |> OpenApiSpexTest.Router.call([])
-
-      assert conn.status == 400
-      assert conn.resp_body == "Unexpected field: inValid2"
     end
   end
 
