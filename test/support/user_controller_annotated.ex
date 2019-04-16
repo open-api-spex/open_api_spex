eb8ef6014e1d0549cca1dd56c6cf25af30d58dd8
Łukasz Niemier <lukasz@niemier.pl>
2019-04-16 02:02:57 +0200

    feat: ExDocs based documentation generation

---
+++ test/support/user_controller_annotated.ex
@@ -0,0 +1,19 @@
+defmodule OpenApiSpexTest.UserControllerAnnotated do
+  use OpenApiSpex.Controller
+
+  @moduledoc tags: ["Foo"]
+
+  @doc """
+  Endpoint summary
+
+  More docs
+  """
+  @doc parameters: [
+    id: [in: :path, type: :string, required: true]
+  ]
+  @doc responses: [
+    ok: {"Foo document", FooSchema}
+  ]
+  def show, do: :ok
+end
+
