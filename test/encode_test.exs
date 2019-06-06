ff0e676d031eb976e8ff9c441dd4a4f4c31d2271
Yury Gargay <yury.gargay@gmail.com>
2019-06-06 13:50:29 +0200

    Add extensions field inside of root object
    This allows specify [extensions](https://swagger.io/specification/#specificationExtensions).
    One of the useful cases could be setting up `x-tagGroups` for
    [redoc](https://github.com/Redocly/redoc/blob/master/docs/redoc-vendor-extensions.md#x-tagGroups)

--- test/encode_test.exs
+++ test/encode_test.exs
@@ -33,4 +33,36 @@ defmodule OpenApiSpex.EncodeTest do
 
     assert is_nil(decoded["info"]["extensions"])
   end
+
+  test "Vendor extensions x-tagGroups properly encoded" do
+    spec = %OpenApi{
+      info: %Info{
+        title: "Test",
+        version: "1.0.0"
+      },
+      extensions: %{
+        "x-tagGroups" => [
+          %{
+            "name" => "Methods",
+            "tags" => [
+              "Search",
+              "Fetch",
+              "Delete"
+            ]
+          }
+        ]
+      },
+      paths: %{}
+    }
+
+    decoded =
+      OpenApiSpex.resolve_schema_modules(spec)
+      |> Jason.encode!()
+      |> Jason.decode!()
+
+    assert hd(decoded["x-tagGroups"])["name"] == "Methods"
+    assert hd(decoded["x-tagGroups"])["tags"] == ["Search", "Fetch", "Delete"]
+
+    assert is_nil(decoded["extensions"])
+  end
 end
