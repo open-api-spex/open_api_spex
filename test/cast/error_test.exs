0b2b9fc20e5256fae7039ca62a75ab35a08dd7d8
Alistair A. Israel <aisrael@gmail.com>
2019-12-10 18:06:01 +0800

    Fix and test for minLength / maxLength error messages

--- test/cast/error_test.exs
+++ test/cast/error_test.exs
@@ -8,4 +8,19 @@ defmodule OpenApiSpex.Cast.ErrorTest do
       assert Error.path_to_string(error) == "/"
     end
   end
+
+  describe "message" do
+    test "it returns the correct error message for :min_items errors" do
+      assert "Array length 0 is smaller than minItems: 1" ==
+               Error.message(%{reason: :min_items, length: 1, value: []})
+
+      assert "Array length 1 is smaller than minItems: 2" ==
+               Error.message(%{reason: :min_items, length: 2, value: ["one"]})
+    end
+
+    test "it returns the correct error message for :max_items errors" do
+      assert "Array length 2 is larger than maxItems: 1" ==
+               Error.message(%{reason: :max_items, length: 1, value: ["one", "two"]})
+    end
+  end
 end
