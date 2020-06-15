8550e77ec1cbd1cd72f534481d80d8405bdb1f7f
Moxley Stratton <moxley.stratton@gmail.com>
2020-06-15 15:16:27 -0700

    Refactor

--- lib/open_api_spex/json_api_helpers/json_api_document.ex
+++ lib/open_api_spex/json_api_helpers/json_api_document.ex
@@ -1,5 +1,39 @@
 defmodule OpenApiSpex.JsonApiHelpers.JsonApiDocument do
+  alias OpenApiSpex.Schema
+  alias OpenApiSpex.JsonApiHelpers.JsonApiResource
+
   defstruct resource: nil,
             multiple: false,
             title: nil
+
+  def schema(%__MODULE__{} = document) do
+    if not is_binary(document.title) do
+      raise "%JsonApiDocument{} :title is required and must be a string"
+    end
+
+    resource = document.resource
+    resource_item_schema = JsonApiResource.schema(resource)
+
+    resource_schema =
+      if document.multiple do
+        %Schema{type: :array, items: resource_item_schema, title: resource.title <> "List"}
+      else
+        resource_item_schema
+      end
+
+    %Schema{
+      type: :object,
+      properties: %{
+        data: resource_schema
+      },
+      required: [:data],
+      title: document.title <> "Document"
+    }
+  end
+
+  def schema(document_attrs) when is_list(document_attrs) or is_map(document_attrs) do
+    __MODULE__
+    |> struct!(document_attrs)
+    |> schema()
+  end
 end
--- lib/open_api_spex/json_api_helpers/json_api_resource.ex
+++ lib/open_api_spex/json_api_helpers/json_api_resource.ex
@@ -1,6 +1,37 @@
 defmodule OpenApiSpex.JsonApiHelpers.JsonApiResource do
+  alias OpenApiSpex.Schema
+
   defstruct additionalProperties: nil,
             properties: %{},
             required: [],
             title: nil
+
+  def schema(%__MODULE__{} = resource) do
+    if not is_binary(resource.title) do
+      raise "%JsonApiResource{} :title is required and must be a string"
+    end
+
+    %Schema{
+      type: :object,
+      properties: %{
+        id: %Schema{type: :string},
+        type: %Schema{type: :string},
+        attributes: attributes_schema(resource)
+      },
+      required: [:id, :type],
+      title: resource.title <> "Resource"
+    }
+  end
+
+  def attributes_schema(%__MODULE__{} = resource) do
+    if not is_binary(resource.title) do
+      raise "%JsonApiResource{} :title is required and must be a string"
+    end
+
+    %Schema{
+      type: :object,
+      properties: resource.properties,
+      title: resource.title <> "Attributes"
+    }
+  end
 end
