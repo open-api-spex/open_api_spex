7aadda52ee4e2afbfc1c1622801bfd482dde4e98
Mike Buhot <m.buhot@gmail.com>
2020-08-23 19:48:51 +1000

    Update example phoenix app to include update in phoenix resources

--- examples/phoenix_app/lib/phoenix_app/accounts/user.ex
+++ examples/phoenix_app/lib/phoenix_app/accounts/user.ex
@@ -1,9 +1,16 @@
 defmodule PhoenixApp.Accounts.User do
   use Ecto.Schema
+  alias __MODULE__
 
   schema "users" do
     field :name, :string
     field :email, :string
     timestamps()
+  end
+
+  def changeset(data \\ %User{}, params) do
+    data
+    |> Ecto.Changeset.cast(params, [:name, :email])
+    |> Ecto.Changeset.validate_required([:name, :email])
   end
 end
