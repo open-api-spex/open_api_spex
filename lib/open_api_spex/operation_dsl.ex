5e795a206439c4ad85003fda9f43848807c12446
Moxley Stratton <moxley.stratton@gmail.com>
2020-09-04 21:29:45 -0700

    Documentation

--- lib/open_api_spex/operation_dsl.ex
+++ lib/open_api_spex/operation_dsl.ex
@@ -2,11 +2,9 @@ defmodule OpenApiSpex.OperationDsl do
   @moduledoc """
   Macros for defining operation specs and operation tags in a Phoenix controller.
 
-  If you use Elixir Formatter, `:open_api_spex` can be added to the `:import_deps`
-  list in the `.formatter.exs` file of your project to make parentheses of the
-  macros optional.
-
   ## Example
+
+  Here is an example Phoenix controller that uses the OperationDsl Operation specs:
 
       defmodule MyAppWeb.DslController do
         use Phoenix.Controller
@@ -43,12 +41,142 @@ defmodule OpenApiSpex.OperationDsl do
         end
       end
 
+  If you use Elixir Formatter, `:open_api_spex` can be added to the `:import_deps`
+  list in the `.formatter.exs` file of your project to make parentheses of the
+  macros optional.
   """
   alias OpenApiSpex.{Operation, OperationBuilder}
 
   @doc """
   Defines an Operation spec in a controller.
+
+  ## Example
+
+      operation :update,
+        summary: "Update user",
+        parameters: [
+          id: [
+            in: :path,
+            description: "User ID",
+            type: :integer,
+            example: 1001
+          ]
+        ],
+        request_body: {"User params", "application/json", UserParams},
+        responses: [
+          ok: {"User response", "application/json", UserResponse}
+        ]
+
+  ## Options
+
+  These options correlate to the
+  [Operation fields specified in the Open API spec](https://swagger.io/specification/#operation-object).
+  One difference however, is that the fields defined in Open API use `camelCase` naming,
+  while the fields used in `OperationDsl` use `snake_case` to match Elixir's convention.
+
+  - `summary` The operation summary
+  - `parameters` The endpoint's parameters. The syntax for `parameters` can take multiple forms:
+    - The common form is a keyword list, where each key is the parameter name, and the value
+      is a keyword list of options that correlate to the fields in an
+      [Open API Parameter Object](https://swagger.io/specification/#parameter-object).
+      For example:
+
+      ```elixir
+        parameters: [
+          id: [
+            in: :path,
+            description: "User ID",
+            type: :integer,
+            example: 1001
+          ]
+        ]
+      ```
+    - A `parameters` list can also contain references to parameter schemas. There are two
+      ways to do that:
+
+      ```elixir
+      parameters: [
+        "$ref": "#/components/parameters/user_id"
+        # or
+        %OpenApiSpex.Reference{"$ref": "#/components/parameters/user_id"}
+      ]
+      ```
+  - `request_body` The endpoint's request body. There are multiple ways to specifiy a request body:
+    - A three or four-element tuple:
+
+        ```elixir
+        request_body: {
+          "User update request body",
+          "application/json",
+          UserUpdateRequest,
+          required: true
+        }
+        ```
+
+      The tuple consists of the following:
+      1. The description
+      2. The content-type
+      3. An Open API schema. This can be a schema module that implements the
+         `OpenApiSpex.Schema` [behaviour](https://hexdocs.pm/elixir/Module.html#module-behaviour),
+         or an `OpenApiSpex.Schema` struct.
+      4. A optional keyword list of options. There is only one option available,
+         and that is `required: boolean`.
+  - `responses` The endpoint's responses, for each HTTP status code the endpoint may respond with.
+    Multiple syntaxes are supported:
+    - A common syntax is a keyword list, where each key is the textual name of an HTTP status code.
+      For example:
+
+        ```elixir
+        [
+          ok: {"User response", "application/json", User},
+          not_found: {"User not found", "application/json", NotFound}
+        ]
+        ```
+
+      The list of names and their code mappings is defined in
+      `Plug.Conn.Status.code/1`.
+
+    - If a map is used, the keys can either be the same atom keys used
+      in the keyword syntax (`%{ok: ...}`), or they can be integers representing
+      the HTTP status code directly:
+
+        ```elixir
+        responses: %{
+          200 => {"User response", "application/json", User},
+          404 => {"User not found", "application/json", NotFound}
+        }
+        ```
+
+    - Each response can be a three-element tuple:
+
+        ```elixir
+        responses: [
+          ok: {"User response", "application/json", User}
+          # Or
+          ok: {"User response", "application/json", %OpenApiSpex.Schema{...}}
+        ]
+        ```
+
+      This tuple consists of:
+      1. A Description string
+      2. A content-type string
+      3. An Open API schema. This can be a schema module that implements the
+         `OpenApiSpex.Schema` [behaviour](https://hexdocs.pm/elixir/Module.html#module-behaviour),
+         or an `OpenApiSpex.Schema` struct.
+
+    - If the response represents an empty response, the definition value can
+      be a single string representing the response description. For example:
+
+        ```elixir
+        responses: [
+          no_content: "Empty response"
+        ]
+        ```
+
+    - A response can also be defined as an `OpenApiSpex.RequestBody` struct. In fact, all
+      response body syntaxes resolve to this struct.
   """
+  @spec operation(action :: atom, spec :: map) :: any
   defmacro operation(action, spec) do
     quote do
       if !Module.get_attribute(__MODULE__, :operation_defined) do
@@ -70,7 +198,16 @@ defmodule OpenApiSpex.OperationDsl do
 
   @doc """
   Defines a list of tags that all operations in a controller will share.
+
+  ## Example
+
+      tags ["users"]
+
+  All operations defined in the controller will inherit the tags specified
+  by this call. If an operation defines its own tags, the tags in this call
+  will be appended to the operation's tags.
   """
+  @spec tags(tags :: [String.t()]) :: any
   defmacro tags(tags) do
     quote do
       @controller_tags unquote(tags)
