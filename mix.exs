7e691ae15e9bb0051af29bb40b5fa17406d06ac5
Thanabodee Charoenpiriyakij <thanabodee.c@linecorp.com>
2020-11-27 18:06:43 +0700

    Fix .formatter.exs doens't include in published package
    Fixes #305

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule OpenApiSpex.Mixfile do
   defp package() do
     [
       name: "open_api_spex",
-      files: ["lib", "mix.exs", "README.md", "LICENSE", "CHANGELOG.md"],
+      files: ["lib", "mix.exs", "README.md", "LICENSE", "CHANGELOG.md", ".formatter.exs"],
       maintainers: [
         "Mike Buhot (m.buhot@gmail.com)",
         "Moxley Stratton",
