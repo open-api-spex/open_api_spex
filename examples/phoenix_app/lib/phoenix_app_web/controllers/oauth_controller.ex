898b7dd6dc54b5d4ee95b1ec295b0ef4d6388459
Moxley Stratton <moxley.stratton@gmail.com>
2020-12-28 10:54:29 -0800

    Working with GitHub OAuth

--- examples/phoenix_app/lib/phoenix_app_web/controllers/oauth_controller.ex
+++ examples/phoenix_app/lib/phoenix_app_web/controllers/oauth_controller.ex
@@ -2,51 +2,23 @@ defmodule PhoenixAppWeb.OauthController do
   use PhoenixAppWeb, :controller
 
   def access_token(conn, _params) do
-    conn = Plug.Conn.fetch_query_params(conn)
-    query_params = conn.query_params
-    IO.inspect(query_params, label: "query_params")
     {:ok, body, conn} = Plug.Conn.read_body(conn, length: 1_000_000)
     body = URI.decode_query(body)
-    IO.inspect(body, label: "body")
-
-    config =
-      PhoenixAppWeb.Router.swagger_ui_config()
-      |> Map.new()
-      |> OpenApiSpex.Plug.SwaggerUI.supplement_config(conn)
-
-    oauth = config.oauth
-
-    IO.inspect(config, label: "config")
-
-    provider_params = %{
-      client_id: oauth[:client_id],
-      client_secret: nil,
-      code: body["code"],
-      redirect_url: config.oauth2_redirect_url,
-      state: nil
-    }
-
-    IO.inspect(provider_params, label: "provider_params")
+    provider_params = body
 
     provider_response = HTTPoison.post!(
       "https://github.com/login/oauth/access_token",
       Jason.encode!(provider_params),
       [{"content-type", "application/json"}])
 
-    provider_body = Jason.decode!(provider_response.body)
+    provider_body = provider_response.body
 
-    IO.inspect(provider_body, label: "provider_body")
+    provider_body =
+      case provider_body do
+        "{" <> _ -> Jason.decode!(provider_body)
+        _ -> URI.decode_query(provider_body)
+      end
 
-    resp_body =
-      %{
-        access_token: provider_body["access_token"],
-        expires_in: 28800,
-        refresh_token: provider_body["refresh_token"],
-        refresh_token_expires_in: 15724800,
-        scope: "",
-        token_type: "bearer"
-      }
-
-    json(conn, resp_body)
+    json(conn, provider_body)
   end
 end
--- lib/open_api_spex/plug/swagger_ui_oauth2_redirect.ex
+++ lib/open_api_spex/plug/swagger_ui_oauth2_redirect.ex
@@ -10,82 +10,75 @@ defmodule OpenApiSpex.Plug.SwaggerUIOAuth2Redirect do
     <!-- HTML for static distribution bundle build -->
     <!doctype html>
     <html lang="en-US">
-    <title>Swagger UI: OAuth2 Redirect</title>
+      <head>
+        <title>Swagger UI: OAuth2 Redirect</title>
+      </head>
+      <body onload="run()">
+        <script>
+            'use strict';
+            function run() {
+                var oauth2 = window.opener.swaggerUIRedirectOauth2;
+                var sentState = oauth2.state;
+                var redirectUrl = oauth2.redirectUrl;
+                var isValid, qp, arr;
 
-    <body onload="run()">
-    </body>
-
-    </html>
-    <script>
-        'use strict';
-        function run() {
-            prompt("press any key to continue");
-            var oauth2 = window.opener.swaggerUIRedirectOauth2;
-            console.log("oauth2", oauth2)
-            var sentState = oauth2.state;
-            var redirectUrl = oauth2.redirectUrl;
-            var isValid, qp, arr;
-
-            if (/code|token|error/.test(window.location.hash)) {
-                qp = window.location.hash.substring(1);
-            } else {
-                qp = location.search.substring(1);
-            }
-
-            arr = qp.split("&")
-            arr.forEach(function (v, i, _arr) { _arr[i] = '"' + v.replace('=', '":"') + '"'; })
-            qp = qp ? JSON.parse('{' + arr.join() + '}',
-                function (key, value) {
-                    return key === "" ? value : decodeURIComponent(value)
-                }
-            ) : {}
-
-            isValid = qp.state === sentState
-
-            var flow = oauth2.auth.schema.get("flow");
-            console.log("flow", flow);
-            console.log("oauth2.auth.code", oauth2.auth.code);
-            console.log("isValid", isValid);
-
-            if ((flow === "accessCode" || flow === "authorizationCode") && !oauth2.auth.code) {
-                if (!isValid) {
-                    oauth2.errCb({
-                        authId: oauth2.auth.name,
-                        source: "auth",
-                        level: "warning",
-                        message: "Authorization may be unsafe, passed state was changed in server Passed state wasn't returned from auth server"
-                    });
-                }
-
-                if (qp.code) {
-                    delete oauth2.state;
-                    oauth2.auth.code = qp.code;
-                    var callbackOpts1 = { auth: oauth2.auth, redirectUrl: redirectUrl };
-                    console.log("callbackOpts1", callbackOpts1)
-                    oauth2.callback({ auth: oauth2.auth, redirectUrl: redirectUrl });
+                if (/code|token|error/.test(window.location.hash)) {
+                    qp = window.location.hash.substring(1);
                 } else {
-                    let oauthErrorMsg
-                    if (qp.error) {
-                        oauthErrorMsg = "[" + qp.error + "]: " +
-                            (qp.error_description ? qp.error_description + ". " : "no accessCode received from the server. ") +
-                            (qp.error_uri ? "More info: " + qp.error_uri : "");
+                    qp = location.search.substring(1);
+                }
+
+                arr = qp.split("&")
+                arr.forEach(function (v, i, _arr) { _arr[i] = '"' + v.replace('=', '":"') + '"'; })
+                qp = qp ? JSON.parse('{' + arr.join() + '}',
+                    function (key, value) {
+                        return key === "" ? value : decodeURIComponent(value)
+                    }
+                ) : {}
+
+                isValid = qp.state === sentState
+                var flow = oauth2.auth.schema.get("flow");
+
+                if ((flow === "accessCode" || flow === "authorizationCode") && !oauth2.auth.code) {
+                    if (!isValid) {
+                        oauth2.errCb({
+                            authId: oauth2.auth.name,
+                            source: "auth",
+                            level: "warning",
+                            message: "Authorization may be unsafe, passed state was changed in server Passed state wasn't returned from auth server"
+                        });
                     }
 
-                    oauth2.errCb({
-                        authId: oauth2.auth.name,
-                        source: "auth",
-                        level: "error",
-                        message: oauthErrorMsg || "[Authorization failed]: no accessCode received from the server"
-                    });
+                    if (qp.code) {
+                        delete oauth2.state;
+                        oauth2.auth.code = qp.code;
+                        var callbackOpts1 = { auth: oauth2.auth, redirectUrl: redirectUrl };
+                        oauth2.callback({ auth: oauth2.auth, redirectUrl: redirectUrl });
+                    } else {
+                        let oauthErrorMsg
+                        if (qp.error) {
+                            oauthErrorMsg = "[" + qp.error + "]: " +
+                                (qp.error_description ? qp.error_description + ". " : "no accessCode received from the server. ") +
+                                (qp.error_uri ? "More info: " + qp.error_uri : "");
+                        }
+
+                        oauth2.errCb({
+                            authId: oauth2.auth.name,
+                            source: "auth",
+                            level: "error",
+                            message: oauthErrorMsg || "[Authorization failed]: no accessCode received from the server"
+                        });
+                    }
+                } else {
+                    // oauth2.auth.state = oauth2.state;
+                    var callbackOpts2 = { auth: oauth2.auth, token: qp, isValid: isValid, redirectUrl: redirectUrl };
+                    oauth2.callback(callbackOpts2);
                 }
-            } else {
-                var callbackOpts2 = { auth: oauth2.auth, token: qp, isValid: isValid, redirectUrl: redirectUrl };
-                console.log("callbackOpts2", callbackOpts2);
-                oauth2.callback({ auth: oauth2.auth, token: qp, isValid: isValid, redirectUrl: redirectUrl, state: qp.state });
+                window.close();
             }
-            // window.close();
-        }
-    </script>
+        </script>
+      </body>
+    </html>
   """
 
   @impl Plug
@@ -93,10 +86,6 @@ defmodule OpenApiSpex.Plug.SwaggerUIOAuth2Redirect do
 
   @impl Plug
   def call(conn, _opts) do
-    conn = fetch_query_params(conn)
-    query_params = conn.query_params
-    IO.inspect(query_params, label: "query_params")
-
     html = render()
 
     conn
