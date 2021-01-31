b8be7d8e5105d0a2e04c9461fd79081f8fd28737
Sean Tan <sean.tan.ly@gmail.com>
2021-02-01 02:22:52 +0800

    relax requirement on poison preventing version conflicts in library usage (#322)

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule OpenApiSpex.Mixfile do
       {:jason, "~> 1.0", optional: true},
       {:phoenix, "~> 1.3", only: [:dev, :test]},
       {:plug, "~> 1.7"},
-      {:poison, "~> 3.1", optional: true}
+      {:poison, "~> 3.1 or ~> 4.0", optional: true}
     ]
   end
 end
