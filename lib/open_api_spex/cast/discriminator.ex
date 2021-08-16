024e7d206e870ffb8300f194ec6f2ac4b9c46c8a
Igor Macedo Quintanilha <igor.quintanilha@saltpay.co>
2021-08-16 17:00:13 +0100

    fix: cast discriminator when value has atom keys

--- lib/open_api_spex/cast/discriminator.ex
+++ lib/open_api_spex/cast/discriminator.ex
@@ -40,11 +40,11 @@ defmodule OpenApiSpex.Cast.Discriminator do
   defp cast_discriminator(%_{value: value, schema: schema} = ctx) do
     {discriminator_property, mappings} = discriminator_details(schema)
 
-    case Map.pop(value, "#{discriminator_property}") do
-      {"", _} ->
+    case value["#{discriminator_property}"] || value[:"#{discriminator_property}"] do
+      v when v in ["", nil] ->
         error(:no_value_for_discriminator, ctx)
 
-      {discriminator_value, _castable_value} ->
+      discriminator_value ->
         # The cast specified by the composite key (allOf, anyOf, oneOf) MUST succeed
         # or return an error according to the Open API Spec.
         composite_ctx = %{
