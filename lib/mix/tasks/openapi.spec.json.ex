6b9f6d39a0f38f135613dac6238cfa98a86f5967
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2022-03-24 00:07:45 +0000

    Document CLI flags of mix openapi.spec.json

--- lib/mix/tasks/openapi.spec.json.ex
+++ lib/mix/tasks/openapi.spec.json.ex
@@ -1,6 +1,22 @@
 defmodule Mix.Tasks.Openapi.Spec.Json do
   @moduledoc """
   Serialize the given OpenApi spec module to a JSON file.
+
+  ## Examples
+
+      $ mix openapi.spec.json --spec PhoenixAppWeb.ApiSpec apispec.json
+      $ mix openapi.spec.json --spec PhoenixAppWeb.ApiSpec --pretty=true
+      $ mix openapi.spec.json --spec PhoenixAppWeb.ApiSpec --vendor-extensions=false
+
+  ## Command line options
+
+  * `--spec` - The ApiSpec module from which to generate the OpenAPI JSON file
+
+  * `--pretty` - Whether to prettify the generated JSON (defaults to false)
+
+  * `--vendor-extensions` - Whether to include open_api_spex OpenAPI vendor extensions
+    (defaults to true)
+
   """
   use Mix.Task
   require Mix.Generator
