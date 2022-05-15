44897b7503ebcde5e96057fcdbde04a223318798
Luca Corti <luca@fantacast.it>
2022-05-15 17:39:46 +0200

    Disable check on function

--- lib/open_api_spex/cast/string.ex
+++ lib/open_api_spex/cast/string.ex
@@ -54,6 +54,7 @@ defmodule OpenApiSpex.Cast.String do
     ?e,
     ?f
   ]
+  # credo:disable-for-next-line
   def cast(%{
         value:
           <<a1, a2, a3, a4, a5, a6, a7, a8, ?-, b1, b2, b3, b4, ?-, c1, c2, c3, c4, ?-, d1, d2, d3,
