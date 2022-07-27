4c2355b2f7eb8bf75f18070fc024cc9578cc42e9
William Volin <me@williamvolin.com>
2022-07-27 12:11:09 -0700

    dont force people to use 4.0 poison

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule OpenApiSpex.Mixfile do
       {:jason, "~> 1.0", optional: true},
       {:phoenix, "~> 1.3", only: [:dev, :test]},
       {:plug, "~> 1.7"},
-      {:poison, "~> 4.0 or ~> 5.0", optional: true},
+      {:poison, "~> 3.0 or ~> 4.0 or ~> 5.0", optional: true},
       {:ymlr, "~> 2.0", optional: true}
     ]
   end
