b73adca4c8c505d80091954d21acf268fe5b30cb
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2022-10-05 02:54:24 +0300

    Make OpenApiSpex.resolve_schema/2 work with schema modules (#458)
    Fixes https://github.com/open-api-spex/open_api_spex/issues/432

--- test/cast/all_of_test.exs
+++ test/cast/all_of_test.exs
@@ -37,8 +37,26 @@ defmodule OpenApiSpex.CastAllOfTest do
     end
 
     test "allOf, for inheritance schema" do
+      import ExUnit.CaptureIO
+
+      defmodule NamedEntity do
+        require OpenApiSpex
+        alias OpenApiSpex.{Schema}
+
+        OpenApiSpex.schema(%{
+          title: "NamedEntity",
+          description: "Anything with a name",
+          type: :object,
+          properties: %{
+            name: %Schema{type: :string}
+          },
+          required: [:name]
+        })
+      end
+
       schema = %Schema{
         allOf: [
+          NamedEntity,
           %Schema{
             type: :object,
             properties: %{
@@ -58,10 +76,12 @@ defmodule OpenApiSpex.CastAllOfTest do
         ]
       }
 
-      value = %{id: "e30aee0f-dbda-40bd-9198-6cf609b8b640", bar: "foo"}
+      value = %{id: "e30aee0f-dbda-40bd-9198-6cf609b8b640", bar: "foo", name: "Elizabeth"}
 
-      assert {:ok, %{id: "e30aee0f-dbda-40bd-9198-6cf609b8b640", bar: "foo"}} =
-               cast(value: value, schema: schema)
+      capture_io(:stderr, fn ->
+        assert {:ok, %{id: "e30aee0f-dbda-40bd-9198-6cf609b8b640", bar: "foo"}} =
+                 cast(value: value, schema: schema)
+      end)
     end
   end
 
