d5183518239d30c0213fe10f4adda5252baf8e81
Mike Buhot <m.buhot@gmail.com>
2022-11-23 17:07:33 +1000

    bump version to 3.16

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule OpenApiSpex.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/open-api-spex/open_api_spex"
-  @version "3.15.0"
+  @version "3.16.0"
 
   def project do
     [
