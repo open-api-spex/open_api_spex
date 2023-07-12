918ccce45a542d5de4ea89dc23de6dd5b31a55a6
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-07-12 19:09:59 +0200

    Require Elixir 1.11+ now

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule OpenApiSpex.Mixfile do
     [
       app: :open_api_spex,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
