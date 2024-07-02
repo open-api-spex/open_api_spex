c759ce11df96270ab389f9cc6f441602bd48ff87
Zach Daniel <zachary.s.daniel@gmail.com>
2024-07-02 09:11:40 -0400

    Update for_schema.ex

--- lib/open_api_spex/inspect/for_schema.ex
+++ lib/open_api_spex/inspect/for_schema.ex
@@ -14,7 +14,6 @@ defimpl Inspect, for: OpenApiSpex.Schema do
     infos =
       for %{field: field} = info <- OpenApiSpex.Schema.__info__(:struct),
           Map.has_key?(map, field),
-          field not in [:__struct__, :__exception__],
           do: info
 
      Inspect.Map.inspect(map, Macro.inspect_atom(:literal, OpenApiSpex.Schema), infos, opts)
